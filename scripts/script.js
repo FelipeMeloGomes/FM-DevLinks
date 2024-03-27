4263b600bc4ba84d2f2f42b6f95a15baac810ff0
Felipe Melo <felipemelogomes36@gmail.com>
2024-03-27 18:53:29 -0300

    feat: add function save user theme favorite

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,38 @@
-let isLightMode = false;
+function getTheme() {
+    return localStorage.getItem("theme") === "light";
+}
 
-function toggleMode() {
-    isLightMode = !isLightMode;
-
+// apply theme
+function applyTheme(isLightMode) {
     document.documentElement.classList.toggle("light", isLightMode);
+}
 
+// update theme localStorage
+function updateTheme(isLightMode) {
+    localStorage.setItem("theme", isLightMode ? "light" : "dark");
+}
+
+// update image
+function updateProfileImage(isLightMode) {
     const img = document.querySelector("#profile img");
     img.setAttribute(
         "src",
         isLightMode ? "/assets/avatar-light.webp" : "/assets/avatar.webp"
     );
 }
+
+// Toggle theme
+function toggleMode() {
+    let isLightMode = getTheme();
+
+    isLightMode = !isLightMode;
+
+    applyTheme(isLightMode);
+
+    updateTheme(isLightMode);
+
+    updateProfileImage(isLightMode);
+}
+
+// Apply theme actual localStorage
+applyTheme(getTheme());
